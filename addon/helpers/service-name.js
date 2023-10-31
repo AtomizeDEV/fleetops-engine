204c8ca699176a1a1a279589b61d426d71b55c0a
Winston <winston@atomize.dev>
2023-10-31 15:09:44 +0100

    Update service-name.js

--- addon/helpers/service-name.js
+++ addon/helpers/service-name.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
-import getServiceName from '@fleetbase/utils/get-service-name';
+import getServiceName from '@atomizedev/utils/get-service-name';
 
 export default helper(function serviceName([name]) {
     return getServiceName(name);
